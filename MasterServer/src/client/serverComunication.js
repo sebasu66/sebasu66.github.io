66e1b82d0761fe1c37fbe6e384c39ce05f03ce6a
sebasu66 <sebastianurciuolo@gmail.com>
2024-01-19 03:31:00 -0300

    Update socket.io version and import statement

--- MasterServer/src/client/serverComunication.js
+++ MasterServer/src/client/serverComunication.js
@@ -1,7 +1,5 @@
 
 import * as evt from "./internalCommEvent.js";
-//import socket.io client
-import { io as SocketIOClient, connect } from 'socket.io-client';
 
 class SocketClient {
     constructor(_url = "http://144.126.249.254") {
@@ -19,7 +17,7 @@ class SocketClient {
 
     //Connect to main server to get the list of LGS
     connectToMainServer() {
-        this.mainServerSocket = SocketIOClient(this.mainServerUrl);
+        this.mainServerSocket = io();
         this.mainServerSocket.on('connect', () => {
             this.connectStatus.connectedToLobby = true;
             evt.dispatchEvent(evt.eventNames.connected_to_main_server, this.connectStatus);
@@ -69,7 +67,7 @@ class SocketClient {
         }
        
         console.log("connecting to game server: " + this.gameServerUrl);
-        this.gameServerSocket = SocketIOClient(this.gameServerUrl);
+        this.gameServerSocket = io(this.gameServerUrl);
 
         this.gameServerSocket.on('connect', () => {
             this.connectStatus.connectedToGame = true;
